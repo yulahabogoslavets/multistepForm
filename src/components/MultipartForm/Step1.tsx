fd39e0a1e611a99672291328eea4755284c64b19
yulahabogoslavets <test@test.email>
2025-05-07 13:56:36 +0200

    feat: use the daisy component

--- src/components/MultipartForm/Step1.tsx
+++ src/components/MultipartForm/Step1.tsx
@@ -12,29 +12,62 @@ export function Step1({ nextStep, onInputChange, values }: Step1Props) {
   return (
     <>
       <form>
-        <input
-          type="text"
-          placeholder="First name"
-          name="firstname"
-          defaultValue={values.firstname}
-          onChange={onInputChange}
-        />
-        <input
-          type="text"
-          placeholder="Last name"
-          name="lastname"
-          defaultValue={values.lastname}
-          onChange={onInputChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          defaultValue={values.email}
-          onChange={onInputChange}
-        />
+        <fieldset
+          className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+          aria-labelledby="step1-legend"
+        >
+          <legend id="step1-legend" className="fieldset-legend">
+            Step 1
+          </legend>
+
+          <label className="floating-label" id="firstname-label">
+            <span>First name</span>
+            <input
+              type="text"
+              className="input mb-4"
+              placeholder="First name"
+              name="firstname"
+              defaultValue={values.firstname}
+              onChange={onInputChange}
+              aria-required="true"
+              aria-labelledby="firstname-label"
+            />
+          </label>
+
+          <label className="floating-label" id="lastname-label">
+            <span>Last name</span>
+            <input
+              type="text"
+              className="input mb-4"
+              placeholder="Last name"
+              name="lastname"
+              defaultValue={values.lastname}
+              onChange={onInputChange}
+              aria-required="true"
+              aria-labelledby="lastname-label"
+            />
+          </label>
+
+          <label className="floating-label" id="email-label">
+            <span>E-Mail</span>
+            <input
+              type="email"
+              className="input"
+              placeholder="E-mail"
+              name="email"
+              defaultValue={values.email}
+              onChange={onInputChange}
+              aria-required="true"
+              aria-labelledby="email-label"
+            />
+          </label>
+        </fieldset>
       </form>
-      <button onClick={nextStep}>Next</button>
+      <div className="flex justify-end mt-4">
+        <button onClick={nextStep} className="btn btn-neutral text-white"  aria-label="Go to the next step">
+          Next
+        </button>
+      </div>
     </>
   );
 }
--- src/components/MultipartForm/Step3.tsx
+++ src/components/MultipartForm/Step3.tsx
@@ -14,19 +14,59 @@ interface Step3Props {
 export function Step3({ prevStep, values, onSubmitHandler }: Step3Props) {
   return (
     <>
-      <h2>Confirm date</h2>
-      <ul>
-        <li>First name: {values.firstname}</li>
-        <li>Last name: {values.lastname}</li>
-        <li>E-mail: {values.email}</li>
-        <li>Street: {values.street}</li>
-        <li>Town: {values.town}</li>
-        <li>Zip: {values.zip}</li>
+      <h2 className="text-2xl italic font-medium mb-2 uppercase">
+        Confirm date
+      </h2>
+      <ul className="list-disc">
+        <li>
+          First name:{' '}
+          <span className="font-semibold">
+            {values.firstname ? values.firstname : '-'}
+          </span>
+        </li>
+        <li>
+          Last name:{' '}
+          <span className="font-semibold">
+            {values.lastname ? values.lastname : '-'}
+          </span>
+        </li>
+        <li>
+          E-mail:{' '}
+          <span className="font-semibold">
+            {values.email ? values.email : '-'}
+          </span>
+        </li>
+        <li>
+          Street:{' '}
+          <span className="font-semibold">
+            {values.street ? values.street : '-'}
+          </span>
+        </li>
+        <li>
+          Town:{' '}
+          <span className="font-semibold">
+            {values.town ? values.town : '-'}
+          </span>
+        </li>
+        <li>
+          Zip:{' '}
+          <span className="font-semibold">{values.zip ? values.zip : '-'}</span>
+        </li>
       </ul>
-      <button onClick={prevStep}>Prev</button>
-      <form onSubmit={onSubmitHandler}>
-        <button type="submit">Send</button>
-      </form>
+      <div className="flex justify-between items-center gap-4 mt-4">
+        <button
+          onClick={prevStep}
+          className="btn btn-outline btn-neutral"
+          aria-label="Go to the previous step"
+        >
+          Prev
+        </button>
+        <form onSubmit={onSubmitHandler}>
+          <button type="submit" className="btn btn-success text-white">
+            Send
+          </button>
+        </form>
+      </div>
     </>
   );
 }
